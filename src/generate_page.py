275ae46db2603bc106b741bb390fc6d93e6922aa
ssuish <quidor.adrean@gmail.com>
2026-03-17 13:28:43 +0800

    bug fix

--- src/generate_page.py
+++ src/generate_page.py
@@ -6,7 +6,7 @@ def extract_title(md):
     lines = md.split("\n")
     for line in lines:
         if line.startswith("# "):
-            return line[2:].strip(" ")
+            return line[2:].strip()
     raise ValueError("There's no title")
 
 
